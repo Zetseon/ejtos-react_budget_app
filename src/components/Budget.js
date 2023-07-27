154a8f59d26a97700e952cae9fc2890c571cc89a
zetseon <parthshah3475@gmail.com>
2023-07-27 19:35:45 -0400

    task 1 final project complete

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,26 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, dispatch } = useContext(AppContext);
+    const [value, setValue] = useState(budget)
+    const handleBudgetChange = (e) => {
+        console.log(e.target.max)
+        const newBudget = e.target.value;
+        console.log(newBudget)
+        if(newBudget > 20000) {
+            alert(`Budget cannot exceed ${e.target.max}`)
+        } else {
+            setValue(newBudget)
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: newBudget
+            })
+        }
+    }
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £<input type='number' required step={10} value={value} max='20000' onChange={handleBudgetChange} /></span>
         </div>
     );
 };
